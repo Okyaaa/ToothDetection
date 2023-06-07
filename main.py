0756433f1dc9e72a6d35c2bb67a2a81d8d5555ee
User <abi.mofra1@gmail.com>
2023-06-07 19:28:15 +0700

    fix upload images

--- main.py
+++ main.py
@@ -1,9 +1,65 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
+import os
+import subprocess
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', }
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
-def hello_world():
-   return render_template('main.html')
+def home():
+    return render_template('main.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('main.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+ 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+
+# def predict():
+#     if request.method == "POST":
+#         if "file" not in request.files:
+#             return redirect(request.url)
+#         file = request.files["file"]
+#         if not file:
+#             return
+#         img_bytes = file.read()
+#         img = Image.open(io.BytesIO(img_bytes))
+#         results = model([img])
+#         results.render()  # updates results.imgs with boxes and labels
+#         results.save(save_dir="static/")
+#         return redirect("static/image0.jpg")
+#     return render_template('main.html')
+
 
 if __name__ == '__main__':
-   app.run(debug=True ,port=8080,use_reloader=True)
+    app.run(debug=True, port=8080)
